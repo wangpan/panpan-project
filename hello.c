c16825edba98f9cb0f9d2598d25fe2366105db1f
wangpan <panpan_563@163.com>
2011-07-24 10:34:25 +0800

    first commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define HOW_MANY_TIMES_TO_SAY 10000
+int main(int argc,const char*argv[]) 
+{   int i;
+    for (i = 0; i < HOW_MANY_TIMES_TO_SAY; i++)
+    {
+		printf("Hello World\n");
+    }
+    return 0;
+}
