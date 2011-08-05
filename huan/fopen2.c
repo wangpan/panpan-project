1b37116c922455681f7c85b9f2486134b9d20f90
wangpan <panpan_563@163.com>
2011-08-05 10:31:44 +0800

    fread fwrite 的用法

--- huan/fopen2.c
+++ huan/fopen2.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 int main(int argc, const char *argv[])
 {
     FILE *fp=NULL;
     char str[1024]="this is a c program";
-    char r_str[1024]={0};
+    char r_str[1024]="shuhssssssssssssssssssssssssssssss";
 //    printf("%s\n",str);
 //    fp = fopen("1.txt","a+");//a+追加 不会破坏之前的东西；w+ 先清空在㝍；r+直接在原来的上面写
     fp =fopen("2.txt","r+");//w+ 可以重新㝍一个2.txt的文件  r+ 与w+ 的最大区别。
@@ -23,7 +24,7 @@ int main(int argc, const char *argv[])
 //    puts(r_str);
 
 
-    int i = 0;
+/*    int i = 0;
     while(str[i]!='\0')
     {
         putc(str[i],fp);//读出str【i】中的内容。
@@ -38,6 +39,14 @@ int main(int argc, const char *argv[])
         i++;
     }
     r_str[i]='\0';
+    printf("%s\n",r_str);*/
+
+
+    int n = 0;
+    fwrite(str,1,strlen(str),fp);//str指针指向要存放的空间 1：控制对象大小 strlen（str）多少个对象 实现功能：将fp中的内容写入到str中 实现方式：以一个字节的方式 读strlen(str)次
+    fseek(fp,0,SEEK_SET);//实现功能：复位
+    n = fread(r_str,1,1023,fp);//读出r_str中所有的内容 1023为r_str的大小
+    r_str[n]='\0';//结尾标志  免得读出r_str中其余的内容  让fp的内容完全覆盖r_str的内容
     printf("%s\n",r_str);
     fclose(fp);//关闭fp。
     return 0;
